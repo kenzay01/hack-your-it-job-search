b3de68b2062c15c48789a057644b826f5012d127
MDR <imdanya946@gmail.com>
2025-07-21 16:37:15 +0300

    remove for some time startSection

--- components/PricingSection.tsx
+++ components/PricingSection.tsx
@@ -139,13 +139,13 @@ const PricingSection = () => {
               <h3 className="text-2xl font-bold text-center mb-4 uppercase">
                 {tariff.name}
               </h3>
-              <div className="font-semibold text-center mb-4 ml-4 flex items-end justify-center space-x-1">
+              <div className="font-semibold text-center mb-4 mr-1 flex items-end justify-center space-x-1">
                 <span className="text-4xl text-transparent bg-clip-text bg-gradient-to-l from-[var(--main-color)] to-[var(--main-two-color)] drop-shadow-sm">
-                  {tariff.discountPrice}
-                </span>
-                <span className="line-through text-gray-400">
                   {tariff.price}
                 </span>
+                {/* <span className="line-through text-gray-400">
+                  {tariff.price}
+                </span> */}
               </div>
               <ul className="space-y-2 md:space-y-4 mb-6">
                 {tariff.features.map((feature, idx) => (
